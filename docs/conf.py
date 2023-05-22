c47601a5c209063a1e9d7f019da97d7d99da44de
Landon Bouma <landonb@users.noreply.github.com>
2023-05-21 19:11:32 -0500

    Bugfix: Remove duplicate docs build 'exclude_patterns'

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,10 @@ version = '.'.join(release.split('.')[:2])
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+#
+# ISOFF: easy-as-pypi: Don't redefine what's defined above:
+#
+#  exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
